af5c3946184d64f4e9ba7daffadb3b76f6e61c3d
wangzs <my_xiaoyufamily@163.com>
2020-01-09 14:55:12 +0800

    refactor: 修改

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,12 +3,14 @@ module.exports = {
   env: {
     node: true
   },
-  extends: ["plugin:vue/essential", "@vue/prettier"],
-  rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
-  },
+  extends: ["plugin:vue/essential", "eslint:recommended", "prettier"],
   parserOptions: {
     parser: "babel-eslint"
+  },
+  rules: {
+    "no-console": 0, //禁止使用console
+    eqeqeq: 0, //必须使用全等
+    "no-debugger": 1, //禁止使用debugger
+    "no-unused-vars": [1, { vars: "all", args: "after-used" }] //不能有声明后未被使用的变量或参数
   }
 };
--- commitlint.config.js
+++ commitlint.config.js
@@ -1,11 +1,7 @@
 module.exports = {
-    extends: ['@commitlint/config-angular'],
-    rules: {
-        'type-enum': [2, 'always', [
-        "feat", "fix", "docs", "style", "refactor", "test", "chore", "revert" , "upgrade","revert","build"
-        ]],
-        // 'subject-full-stop': [0, 'never'],
-        // 'subject-case': [0, 'never']
-        "type-case": [0, "always", "lowerCase"]
-    }
+  extends: ["@commitlint/config-angular"],
+  rules: {
+    "type-enum": [2, "always", ["feat", "fix", "docs", "style", "refactor", "test", "chore", "revert", "upgrade", "revert", "build"]],
+    "type-case": [0, "always", "lowerCase"]
+  }
 };
